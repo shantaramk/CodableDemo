03a8250fb0fc1d1385884da8232baf00b8de6e1c
Shantaram K <shantaramk@Mipladmin-67.local>
2019-11-06 12:12:40 +0530

    first commit

---
+++ CodableDemo/Cell/CountryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CountryCell.swift
+//  CodableDemo
+//
+//  Created by Shantaram K on 06/11/19.
+//  Copyright © 2019 Shantaram K. All rights reserved.
+//
+
+import UIKit
+
+class CountryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
